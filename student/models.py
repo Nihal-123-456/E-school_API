784d6e018df8f7f8c6a13a7e4e1f4c6511c31d34
unknown <you@example.com>
2024-02-23 22:58:31 +0600

    rating datatype changed

--- student/models.py
+++ student/models.py
@@ -15,17 +15,9 @@ class CartModel (models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(CourseModel, on_delete=models.CASCADE)
 
-RATINGS = {
-    ('⭐','1'),
-    ('⭐⭐','2'),
-    ('⭐⭐⭐','3'),
-    ('⭐⭐⭐⭐','4'),
-    ('⭐⭐⭐⭐⭐','5'),
-}
-
 class ReviewModel (models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(CourseModel, on_delete=models.CASCADE)
     comment = models.TextField()
-    rating = models.CharField(choices=RATINGS, max_length=20)
+    rating = models.IntegerField()
     creation_date = models.DateField(auto_now_add=True)
