856b981e18f8f614dfdf5ba5d66b6f54ec01ea10
unknown <you@example.com>
2024-03-02 12:05:07 +0600

    topic name changed to string

--- teacher/serializers.py
+++ teacher/serializers.py
@@ -7,7 +7,26 @@ class TopicSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CourseSerializer(serializers.ModelSerializer):
-    topics_name = serializers.StringRelatedField(source='topics', many=True)
+    topics = serializers.SlugRelatedField(
+        queryset=TopicModel.objects.all(),
+        slug_field='name',
+        many=True
+    )
+
     class Meta:
         model = CourseModel
-        fields = ['id','teacher', 'image', 'title', 'topics', 'topics_name', 'description', 'price', 'discount', 'date']
+        fields = '__all__'
+
+    def to_internal_value(self, data):
+        topics_data = data.pop('topics', None)
+        instance = super().to_internal_value(data)
+        if topics_data is not None:
+            topics = []
+            for topic_name in topics_data:
+                try:
+                    topic = TopicModel.objects.get(name=topic_name)
+                    topics.append(topic)
+                except TopicModel.DoesNotExist:
+                    raise serializers.ValidationError(f"Topic with slug '{topic_name}' does not exist.")
+            instance['topics'] = topics
+        return instance
