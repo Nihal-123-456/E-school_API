239a7ba7d2391fca73bfdad4742f415ea98fa927
unknown <you@example.com>
2024-03-11 15:04:35 +0600

    activation link added

--- user/views.py
+++ user/views.py
@@ -42,7 +42,7 @@ class RegistrationView(APIView):
             user = serializer.save()
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f'http://127.0.0.1:8000/user/active/{uid}/{token}'
+            confirm_link = f'https://e-school-api.onrender.com/user/active/{uid}/{token}'
             email_subject = 'Confirm Email'
             email_body = render_to_string('confirm_mail.html',{'confirm_link':confirm_link})
             email = EmailMultiAlternatives(email_subject, '', to=[user.email])
